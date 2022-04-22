d13b8a091c2ae1bf93e4585c96fe320fc0b63e97
Tong Quang Truong <truongwerk@gmail.com>
2022-04-22 15:36:46 +0700

    funtional Compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,43 @@
-import React from "react";
+import React, { useState,useRef } from "react";
 import Input from "./components/Input";
 import CvDemo from "./components/Cv";
 import ReactToPrint from "react-to-print";
-
 import Button from "@mui/material/Button";
 import SaveAsOutlinedIcon from "@mui/icons-material/SaveAsOutlined";
 import "./styles/app.css";
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.printCV = React.createRef()
-		this.state = {
-			name: "",
-			profile: "",
-			position: "",
-			description: "",
-			address: "",
-			phone: "",
-			email: "",
-			website: "",
-			skill: [],
-			education: [],
-			experience: [],
-		};
-	}
-	handleInputCallback = (childData, selector) => {
-		this.setState({ [selector]: childData });
+const App = () => {
+	const [state, setState] = useState({
+		name: "Name",
+		profile: "",
+		position: "Position",
+		description:
+			"Cupcake shortbread cake danish chocolate chocolate cake chocolate bar. Powder carrot cake tiramisu marzipan donut cotton candy toffee chocolate. Chupa chups powder tiramisu macaroon danish caramels gummi bears.",
+		address: "A Street, B Town, C Country",
+		phone: "123456789",
+		email: "abc@gmail.com",
+		website: "abc.com",
+		skill: ["HTML", "CSS", "Javascript", "React"],
+		education: [
+			{ name: "School A", major: "Math", year: "2015 - 2016" },
+			{ name: "School B", major: "English", year: "2016 - 2017" },
+		],
+		experience: [
+			{ name: "Company A", position: "Font-End", year: "2017 - 2018" },
+			{ name: "Company B", position: "Back-End", year: "2018 - Present" },
+		],
+	});
+	const handleInputCallback = (childData, selector) => {
+		setState({ ...state, [selector]: childData });
 	};
-	render() {
-		const cvData = this.state;
-		return (
-			<div id="App">
-				<header>
-					<h1>CV Application</h1>
-					<p>
-						© 2022 Create by{" "}
-						<a
-							href="https://github.com/truongwerk"
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							Tong Quang Truong
-						</a>
-					</p>
-				</header>
-				<div id="content">
-					<div id="inputForm">
-						<Input parentCallback={this.handleInputCallback} />
-						<ReactToPrint
+  const printRef = useRef()
+	return (
+		<div id="App">
+			<Header />
+			<div id="content">
+				<div id="inputForm">
+					<Input parentCallback={handleInputCallback} />
+          <ReactToPrint
 							trigger={() => {
 								return (
 									<Button id="printBtn" fullWidth variant="outlined" endIcon={<SaveAsOutlinedIcon />}>
@@ -56,16 +45,33 @@ class App extends React.Component {
 									</Button>
 								);
 							}}
-							content={() => this.printCV.current}
+							content={() => printRef.current}
 						/>
-					</div>
-					<div id="cvWrapper"ref={this.printCV}>
-						<CvDemo cvData={cvData}  />
-					</div>
+				</div>
+				<div id="cvWrapper" ref={printRef}>
+					<CvDemo cvData={state} />
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+const Header = () => {
+	return (
+		<header>
+			<h1>CV Application</h1>
+			<p>
+				© 2022 Create by{" "}
+				<a
+					href="https://github.com/truongwerk"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					Tong Quang Truong
+				</a>
+			</p>
+		</header>
+	);
+};
 
 export default App;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import TextField from "@mui/material/TextField";
 import { styled } from "@mui/material/styles";
@@ -14,413 +14,356 @@ const InputFile = styled("input")({
 	display: "none",
 });
 
-class Input extends React.Component {
-	inputChange = (e) => {
+const Input = (props) => {
+	const inputChange = (e) => {
 		const selector = e.target.id.replace("input", "").toLowerCase();
-		this.props.parentCallback(e.target.value, selector);
+		props.parentCallback(e.target.value, selector);
 	};
-	inputProfile = (e) => {
+	const inputProfile = (e) => {
 		const selector = e.target.id.replace("input", "").toLowerCase();
-		this.props.parentCallback(URL.createObjectURL(e.target.files[0]), selector);
+		props.parentCallback(URL.createObjectURL(e.target.files[0]), selector);
 	};
-	inputMode = (value, mode) => {
-		this.props.parentCallback(value, mode);
+	const inputMode = (value, mode) => {
+		props.parentCallback(value, mode);
 	};
-	render() {
-		return (
-			<>
-				<div id="basicInput">
-					<h2>Information</h2>
-					<TextField
-						label="Name"
-						fullWidth
-						size="small"
-						onChange={this.inputChange}
-						id="inputName"
-						className="longInput"
+	return (
+		<>
+			<div id="basicInput">
+				<h2>Information</h2>
+				<TextField
+					label="Name"
+					fullWidth
+					size="small"
+					onChange={inputChange}
+					id="inputName"
+					className="longInput"
+				/>
+				<label htmlFor="inputProfile">
+					<InputFile
+						accept="image/*"
+						id="inputProfile"
+						type="file"
+						onChange={inputProfile}
 					/>
-					<label htmlFor="inputProfile">
-						<InputFile
-							accept="image/*"
-							id="inputProfile"
-							type="file"
-							onChange={this.inputProfile}
-						/>
-						<Button
-							variant="outlined"
-							fullWidth
-							component="span"
-							endIcon={<PhotoCamera />}
-							id="profileLabel"
-						>
-							Upload Profile Picture
-						</Button>
-					</label>
-					<TextField
-						label="Position"
+					<Button
+						variant="outlined"
 						fullWidth
-						size="small"
-						onChange={this.inputChange}
-						id="inputPosition"
-						className="longInput"
-					/>
-					<TextField
-						label="Description"
-						fullWidth
-						size="small"
-						onChange={this.inputChange}
-						multiline
-						rows={4}
-						id="inputDescription"
-						className="longInput"
-					/>
-					<h2>Contact</h2>
-					<TextField
-						label="Address"
-						fullWidth
-						size="small"
-						onChange={this.inputChange}
-						id="inputAddress"
-						className="longInput"
-					/>
-					<TextField
-						label="Phone"
-						fullWidth
-						size="small"
-						onChange={this.inputChange}
-						id="inputPhone"
-						className="longInput"
-					/>
-					<TextField
-						label="Email"
-						fullWidth
-						size="small"
-						onChange={this.inputChange}
-						id="inputEmail"
-						className="longInput"
-					/>
-					<TextField
-						label="Website"
-						fullWidth
-						size="small"
-						onChange={this.inputChange}
-						id="inputWebsite"
-						className="longInput"
-					/>
-				</div>
-				<SkillInput inputCallback={this.inputMode} />
-				<EducationInput inputCallback={this.inputMode} />
-				<ExperienceInput inputCallback={this.inputMode} />
-			</>
-		);
-	}
-}
+						component="span"
+						endIcon={<PhotoCamera />}
+						id="profileLabel"
+					>
+						Upload Profile Picture
+					</Button>
+				</label>
+				<TextField
+					label="Position"
+					fullWidth
+					size="small"
+					onChange={inputChange}
+					id="inputPosition"
+					className="longInput"
+				/>
+				<TextField
+					label="Description"
+					fullWidth
+					size="small"
+					onChange={inputChange}
+					multiline
+					rows={4}
+					id="inputDescription"
+					className="longInput"
+				/>
+				<h2>Contact</h2>
+				<TextField
+					label="Address"
+					fullWidth
+					size="small"
+					onChange={inputChange}
+					id="inputAddress"
+					className="longInput"
+				/>
+				<TextField
+					label="Phone"
+					fullWidth
+					size="small"
+					onChange={inputChange}
+					id="inputPhone"
+					className="longInput"
+				/>
+				<TextField
+					label="Email"
+					fullWidth
+					size="small"
+					onChange={inputChange}
+					id="inputEmail"
+					className="longInput"
+				/>
+				<TextField
+					label="Website"
+					fullWidth
+					size="small"
+					onChange={inputChange}
+					id="inputWebsite"
+					className="longInput"
+				/>
+			</div>
+			<SkillInput inputCallback={inputMode} />
+			<EducationInput inputCallback={inputMode} />
+			<ExperienceInput inputCallback={inputMode} />
+		</>
+	);
+};
 Input.propTypes = {
 	parentCallback: PropTypes.func,
 };
 
-class SkillInput extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			inputSkill: [],
-		};
-	}
-	addSkill = () => {
-		this.setState({ inputSkill: this.state.inputSkill.concat("") });
-		this.updateCv();
+const SkillInput = (props) => {
+	const [inputSkill, setInputSkill] = useState([]);
+	const addSkill = () => {
+		setInputSkill(inputSkill.concat(""));
 	};
-	deleteSkill = (i) => {
-		this.setState((state) => {
-			const inputSkill = state.inputSkill.filter((item, index) => index !== i);
-			return { inputSkill };
-		});
+	const deleteSkill = (i) => {
+		setInputSkill(inputSkill.filter((item, index) => index !== i));
+	};
+	const changeSkill = (index, value) => {
+		const newInputSkill = inputSkill;
+		newInputSkill[index] = value;
+		setInputSkill(newInputSkill);
+	};
+	useEffect(() => {
+		if (inputSkill.length === 0) {
+			props.inputCallback(["HTML", "CSS", "JavaScript", "React"], "skill");
+		} else {
+			props.inputCallback(inputSkill, "skill");
+		}
+	}, [props]);
 
-		this.updateCv();
-	};
-	changeSkill = (index, value) => {
-		this.setState((state) => {
-			const inputSkill = state.inputSkill.map((item, j) => {
-				if (j === index) {
-					return value;
-				} else {
-					return item;
-				}
-			});
-			return {
-				inputSkill,
-			};
-		});
+	return (
+		<div id="skillInput">
+			<h2>Skill</h2>
+			{inputSkill.map((element, index) => {
+				return (
+					<div key={index} className="skills">
+						<TextField
+							id="outlined-basic"
+							label={`Skill ${index + 1}`}
+							variant="outlined"
+							size="small"
+							value={element}
+							onChange={(e) => changeSkill(index, e.target.value)}
+						/>
+						<img
+							src={deleteIcon}
+							alt="deleteIcon"
+							className="deleteIcon"
+							onClick={() => deleteSkill(index)}
+						/>
+					</div>
+				);
+			})}
+			<IconButton
+				aria-label="addCircleIcon"
+				size="large"
+				id="addSkill"
+				onClick={addSkill}
+			>
+				<AddCircleIcon fontSize="inherit" />
+			</IconButton>
+		</div>
+	);
+};
+SkillInput.propTypes = {
+	inputCallback: PropTypes.func,
+};
 
-		this.updateCv();
+const EducationInput = (props) => {
+	const [inputSchool, setInputSchool] = useState([]);
+	const addSchool = () => {
+		setInputSchool(
+			inputSchool.concat({
+				name: "",
+				major: "",
+				year: "",
+			})
+		);
 	};
-	updateCv = () => {
-		setTimeout(() => {
-			this.props.inputCallback(this.state.inputSkill, "skill");
-		});
+	const deleteSchool = (i) => {
+		setInputSchool(inputSchool.filter((element, index) => index !== i));
 	};
-
-	render() {
-		return (
-			<div id="skillInput">
-				<h2>Skill</h2>
-				{this.state.inputSkill.map((element, index) => {
-					return (
-						<div key={index} className="skills">
+	const changeSchool = (i, value, mode) => {
+		const newInputSchool = inputSchool;
+		newInputSchool[i] = { ...newInputSchool[i], [mode]: value };
+		setInputSchool(newInputSchool);
+	};
+	useEffect(() => {
+		if (inputSchool.length === 0) {
+			props.inputCallback(
+				[
+					{ name: "School A", major: "Math", year: "2015 - 2016" },
+					{ name: "School B", major: "English", year: "2016 - 2017" },
+				],
+				"education"
+			);
+		} else {
+			props.inputCallback(inputSchool, "education");
+		}
+	}, [inputSchool]);
+	return (
+		<div id="educationInput">
+			<h2>Education</h2>
+			{inputSchool.map((element, index) => {
+				return (
+					<div key={index} className="schoolInput">
+						<TextField
+							label={`School ${index + 1}`}
+							fullWidth
+							value={element.name}
+							className="longInput"
+							size="small"
+							onChange={(e) => {
+								changeSchool(index, e.target.value, "name");
+							}}
+						/>
+						<TextField
+							label={`Major ${index + 1}`}
+							fullWidth
+							value={element.major}
+							className="longInput"
+							size="small"
+							onChange={(e) => {
+								changeSchool(index, e.target.value, "major");
+							}}
+						/>
+						<div>
 							<TextField
-								id="outlined-basic"
-								label={`Skill ${index + 1}`}
-								variant="outlined"
+								label={`Year ${index + 1}`}
+								fullWidth
+								value={element.year}
+								className="shortInput"
 								size="small"
-								value={element}
-								onChange={(e) => this.changeSkill(index, e.target.value)}
+								onChange={(e) => {
+									changeSchool(index, e.target.value, "year");
+								}}
 							/>
 							<img
 								src={deleteIcon}
 								alt="deleteIcon"
 								className="deleteIcon"
-								onClick={() => this.deleteSkill(index)}
+								onClick={() => {
+									deleteSchool(index);
+								}}
 							/>
 						</div>
-					);
-				})}
-				<IconButton
-					aria-label="addCircleIcon"
-					size="large"
-					id="addSkill"
-					onClick={this.addSkill}
-				>
-					<AddCircleIcon fontSize="inherit" />
-				</IconButton>
-			</div>
-		);
-	}
-}
-SkillInput.propTypes = {
-	inputCallback: PropTypes.func,
+					</div>
+				);
+			})}
+			<IconButton
+				aria-label="addCircleIcon"
+				size="large"
+				id="addSchool"
+				onClick={addSchool}
+			>
+				<AddCircleIcon fontSize="inherit" />
+			</IconButton>
+		</div>
+	);
 };
-
-class EducationInput extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			inputSchool: [],
-		};
-	}
-	addSchool = () => {
-		this.setState({
-			inputSchool: this.state.inputSchool.concat({
-				name: "",
-				major: "",
-				year: "",
-			}),
-		});
-		this.updateCv();
-	};
-	deleteSchool = (i) => {
-		this.setState((state) => {
-			const inputSchool = state.inputSchool.filter(
-				(element, index) => index !== i
-			);
-			return { inputSchool };
-		});
-		this.updateCv();
-	};
-	changeSchool = (i, value, mode) => {
-		this.setState((state) => {
-			const inputSchool = state.inputSchool.map((element, index) => {
-				if (index === i) {
-					return { ...element, [mode]: value };
-				} else {
-					return element;
-				}
-			});
-			return {
-				inputSchool,
-			};
-		});
-		this.updateCv();
-	};
-	updateCv = () => {
-		setTimeout(() => {
-			this.props.inputCallback(this.state.inputSchool, "education");
-		});
-	};
-
-	render() {
-		return (
-			<div id="educationInput">
-				<h2>Education</h2>
-				{this.state.inputSchool.map((element, index) => {
-					return (
-						<div key={index} className="schoolInput">
-							<TextField
-								label={`School ${index + 1}`}
-								fullWidth
-								value={element.name}
-								className="longInput"
-								size="small"
-								onChange={(e) => {
-									this.changeSchool(index, e.target.value, "name");
-								}}
-							/>
-							<TextField
-								label={`Major ${index + 1}`}
-								fullWidth
-								value={element.major}
-								className="longInput"
-								size="small"
-								onChange={(e) => {
-									this.changeSchool(index, e.target.value, "major");
-								}}
-							/>
-							<div>
-								<TextField
-									label={`Year ${index + 1}`}
-									fullWidth
-									value={element.year}
-									className="shortInput"
-									size="small"
-									onChange={(e) => {
-										this.changeSchool(index, e.target.value, "year");
-									}}
-								/>
-								<img
-									src={deleteIcon}
-									alt="deleteIcon"
-									className="deleteIcon"
-									onClick={() => {
-										this.deleteSchool(index);
-									}}
-								/>
-							</div>
-						</div>
-					);
-				})}
-				<IconButton
-					aria-label="addCircleIcon"
-					size="large"
-					id="addSchool"
-					onClick={this.addSchool}
-				>
-					<AddCircleIcon fontSize="inherit" />
-				</IconButton>
-			</div>
-		);
-	}
-}
 EducationInput.propTypes = {
 	inputCallback: PropTypes.func,
 };
 
-class ExperienceInput extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			inputCompany: [],
-		};
-	}
-	addCompany = () => {
-		this.setState({
-			inputCompany: this.state.inputCompany.concat({
+const ExperienceInput = (props) => {
+	const [inputCompany, setInputCompany] = useState([]);
+	const addCompany = () => {
+		setInputCompany(
+			inputCompany.concat({
 				name: "",
 				position: "",
 				year: "",
-			}),
-		});
-		this.updateCv();
-	};
-	deleteCompany = (i) => {
-		this.setState((state) => {
-			const inputCompany = state.inputCompany.filter(
-				(element, index) => index !== i
-			);
-			return { inputCompany };
-		});
-		this.updateCv();
-	};
-	changeCompany = (i, value, mode) => {
-		this.setState((state) => {
-			const inputCompany = state.inputCompany.map((element, index) => {
-				if (index === i) {
-					return { ...element, [mode]: value };
-				} else {
-					return element;
-				}
-			});
-			return {
-				inputCompany,
-			};
-		});
-		this.updateCv();
-	};
-	updateCv = () => {
-		setTimeout(() => {
-			this.props.inputCallback(this.state.inputCompany, "experience");
-		});
-	};
-	render() {
-		return (
-			<div id="experienceInput">
-				<h2>Experience</h2>
-				{this.state.inputCompany.map((element, index) => {
-					return (
-						<div key={index} className="schoolInput">
-							<TextField
-								label={`Company ${index + 1}`}
-								fullWidth
-								value={element.name}
-								className="longInput"
-								size="small"
-								onChange={(e) => {
-									this.changeCompany(index, e.target.value, "name");
-								}}
-							/>
-							<TextField
-								label={`Position ${index + 1}`}
-								fullWidth
-								value={element.position}
-								className="longInput"
-								size="small"
-								onChange={(e) => {
-									this.changeCompany(index, e.target.value, "position");
-								}}
-							/>
-							<div>
-								<TextField
-									label={`Year ${index + 1}`}
-									fullWidth
-									value={element.year}
-									className="shortInput"
-									size="small"
-									onChange={(e) => {
-										this.changeCompany(index, e.target.value, "year");
-									}}
-								/>
-								<img
-									src={deleteIcon}
-									alt="deleteIcon"
-									className="deleteIcon"
-									onClick={() => {
-										this.deleteCompany(index);
-									}}
-								/>
-							</div>
-						</div>
-					);
-				})}
-				<IconButton
-					aria-label="addCircleIcon"
-					size="large"
-					id="addCompany"
-					onClick={this.addCompany}
-				>
-					<AddCircleIcon fontSize="inherit" />
-				</IconButton>
-			</div>
+			})
 		);
-	}
-}
+	};
+	const deleteCompany = (i) => {
+		setInputCompany(inputCompany.filter((element, index) => index !== i));
+	};
+	const changeCompany = (i, value, mode) => {
+		const newInputCompany = inputCompany;
+		newInputCompany[i] = { ...newInputCompany[i], [mode]: value };
+		setInputCompany(newInputCompany);
+	};
+	useEffect(() => {
+		if (inputCompany.length === 0) {
+			props.inputCallback(
+				[
+					{ name: "Company A", position: "Font-End", year: "2017 - 2018" },
+					{ name: "Company B", position: "Back-End", year: "2018 - Present" },
+				],
+				"experience"
+			);
+		} else {
+			props.inputCallback(inputCompany, "experience");
+		}
+	}, [inputCompany]);
+	return (
+		<div id="experienceInput">
+			<h2>Experience</h2>
+			{inputCompany.map((element, index) => {
+				return (
+					<div key={index} className="schoolInput">
+						<TextField
+							label={`Company ${index + 1}`}
+							fullWidth
+							value={element.name}
+							className="longInput"
+							size="small"
+							onChange={(e) => {
+								changeCompany(index, e.target.value, "name");
+							}}
+						/>
+						<TextField
+							label={`Position ${index + 1}`}
+							fullWidth
+							value={element.position}
+							className="longInput"
+							size="small"
+							onChange={(e) => {
+								changeCompany(index, e.target.value, "position");
+							}}
+						/>
+						<div>
+							<TextField
+								label={`Year ${index + 1}`}
+								fullWidth
+								value={element.year}
+								className="shortInput"
+								size="small"
+								onChange={(e) => {
+									changeCompany(index, e.target.value, "year");
+								}}
+							/>
+							<img
+								src={deleteIcon}
+								alt="deleteIcon"
+								className="deleteIcon"
+								onClick={() => {
+									deleteCompany(index);
+								}}
+							/>
+						</div>
+					</div>
+				);
+			})}
+			<IconButton
+				aria-label="addCircleIcon"
+				size="large"
+				id="addCompany"
+				onClick={addCompany}
+			>
+				<AddCircleIcon fontSize="inherit" />
+			</IconButton>
+		</div>
+	);
+};
 ExperienceInput.propTypes = {
 	inputCallback: PropTypes.func,
 };
