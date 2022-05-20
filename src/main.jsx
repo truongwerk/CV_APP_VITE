037289cd1a027d15f6c89ee1bf02f9f68b9b0dde
Tong Quang Truong <truongwerk@gmail.com>
2022-05-20 09:47:05 +0700

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import ReactDOM from "react-dom/client";
 import "./styles/reset.css";
 import App from "./App";
 
@@ -11,9 +10,3 @@ render(
 	</React.StrictMode>,
 	container
 );
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-// 	<React.StrictMode>
-// 		<App />
-// 	</React.StrictMode>
-// );
